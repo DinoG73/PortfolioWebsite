7a7c5c52282fde5401bd50ef164cbbbc658d8ff9
DinoG73 <53279025+DinoG73@users.noreply.github.com>
2023-03-30 20:25:50 -0300

    Add tha acction for change section

--- app.js
+++ app.js
@@ -11,8 +11,29 @@ function PageTransition () {
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn';
-        })
+        });
     }
+
+    //SECTIONS ACTIVE CLASS
+    allSection.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if(id) {
+            //REMOVE SELECTED FROM THE OTHER BUTNS
+            sectBtnsBox.forEach((btn) => {
+                btn.classList.remove('active');
+            });
+            e.target.classList.add('active');
+
+            //HIDE OTHER SECTIONS
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            });
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+        console.log(e.target);
+    });
 }
 
 PageTransition();
